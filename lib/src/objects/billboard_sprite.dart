8de3856ae116defbade4fcae675626c728b0292a
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2023-01-12 23:51:35 +0100

    fix: anchor point of `BillBoardSprite` is off

--- lib/src/objects/billboard_sprite.dart
+++ lib/src/objects/billboard_sprite.dart
@@ -16,15 +16,17 @@ class BillboardSprite extends SashimiObject {
   }) : super(
           size: Vector3(size.x, size.y, 1),
           scale: Vector3(scale?.x ?? 1, scale?.y ?? 1, 1),
-        );
+        ) {
+    controller.anchor = Anchor.bottomCenter;
+  }
 
   /// The sprite to render.
   final Sprite sprite;
 
   @override
   void update(double dt) {
-    // Set the angle to the viewfinder angle and subtract own angle.
-    // This ensures that if angle is set to 0, the sprite will always face the
+    // Set the angle to the viewfinder angle and subtract own angle. This
+    // ensures that if angle is set to 0, the sprite will always face the
     // camera.
     controller.angle = parent.viewfinder.angle - angle;
   }
@@ -41,11 +43,15 @@ class BillboardSprite extends SashimiObject {
 }
 
 class _SashimiSlice extends SashimiSlice<BillboardSprite> {
-  _SashimiSlice({required super.owner});
+  _SashimiSlice({required super.owner}) : super(anchor: Anchor.bottomCenter);
 
   @override
   void update(double dt) {
     super.update(dt);
+
+    // Set the position relative to the position of the controller based on the
+    // anchor (bottom center).
+    position.setFrom(owner.controller.positionOfAnchor(anchor));
 
     // Set the angle to the controller's angle as that one will always be
     // visually correct.

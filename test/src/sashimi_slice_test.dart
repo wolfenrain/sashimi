58038964d6b71b09ecec88a6c9a283b86ab26be8
Jochum van der Ploeg <jochum.vanderploeg@verygood.ventures>
2023-01-21 21:56:55 +0100

    fix: take z-scale into factor when positioning

--- test/src/sashimi_slice_test.dart
+++ test/src/sashimi_slice_test.dart
@@ -10,7 +10,7 @@ class _TestObject extends SashimiObject {
 
   @override
   List<SashimiSlice<SashimiObject>> generateSlices() {
-    return [_TestSlice(owner: this)];
+    return [_TestSlice(owner: this), _TestSlice(owner: this)];
   }
 }
 
@@ -21,22 +21,49 @@ class _TestSlice extends SashimiSlice {
 void main() {
   group('SashimiSlice', () {
     sashimiGame.testGameWidget(
+      'calculate priority based on owner',
+      setUp: (game, tester) => game.ensureAdd(_TestObject()),
+      verify: (game, tester) async {
+        final object = game.descendants().whereType<_TestObject>().first;
+        final firstSlice = object.slices.first;
+        final secondSlice = object.slices.last;
+
+        game.update(0);
+        expect(firstSlice.priority, equals(0));
+        expect(secondSlice.priority, equals(6));
+
+        object.position.setValues(10, 10, 10);
+        object.size.setValues(20, 20, 20);
+
+        game.update(0);
+        expect(firstSlice.priority, equals(10));
+        expect(secondSlice.priority, equals(21));
+      },
+    );
+
+    sashimiGame.testGameWidget(
       'realigns with owner',
       setUp: (game, tester) => game.ensureAdd(_TestObject()),
       verify: (game, tester) async {
         final object = game.descendants().whereType<_TestObject>().first;
-        final slice = object.slices.first;
+        final firstSlice = object.slices.first;
+        final secondSlice = object.slices.last;
 
         game.update(0);
-        expect(slice.position, equals(Vector2.zero()));
-        expect(slice.size, equals(Vector2.all(10)));
+        expect(firstSlice.position, equals(Vector2.zero()));
+        expect(firstSlice.size, equals(Vector2.all(10)));
+        expect(secondSlice.position, equals(Vector2(0, -6)));
+        expect(secondSlice.size, equals(Vector2.all(10)));
 
         object.position.setValues(10, 10, 0);
         object.size.setValues(20, 20, 20);
+        object.scale.setValues(2, 2, 2);
 
         game.update(0);
-        expect(slice.position, equals(Vector2.all(10)));
-        expect(slice.size, equals(Vector2.all(20)));
+        expect(firstSlice.position, equals(Vector2.all(10)));
+        expect(firstSlice.size, equals(Vector2.all(20)));
+        expect(secondSlice.position, equals(Vector2(10, -12)));
+        expect(secondSlice.size, equals(Vector2.all(20)));
       },
     );
 
